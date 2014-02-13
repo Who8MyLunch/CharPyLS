ebf38d5f3f76630fd8c83ba8af458a6c51aea939
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2014-02-13 07:37:09 -0800

    update example to point to existing test png file.

--- jpeg_ls/example.py
+++ jpeg_ls/example.py
@@ -8,11 +8,11 @@ import data_io
 import jpeg_ls
 
 # Read in an image from an existing PNG file.
-fname_img = 'test/image.png'
+fname_img = 'test/gray_raw.png'
 data_image = data_io.read_PIL(fname_img)
 
 # This input image should be a numpy array.
-print('\nData properties')
+print('\nData properties:')
 print('Type:  {:s}'.format(data_image.dtype))
 print('Shape: {:s}'.format(data_image.shape))
 
@@ -31,4 +31,4 @@ data_image_b = jpeg_ls.decode(data_buffer)
 
 # Compare image data, before and after.
 is_same = (data_image == data_image_b).all()
-print('\nRestored data is identical to original?: {:s}'.format(str(is_same)))
+print('\nRestored data is identical to original? {:s}\n'.format(str(is_same)))
